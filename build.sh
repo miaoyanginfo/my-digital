2906002b8e5d84c0beaaa28ee86c6c2308e2b321
miaoyanginfo <paulmiao@me.com>
2024-10-24 08:55:58 +0800

    butter

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ rm -rf source/_posts
 mkdir -p source/_posts
 
 ## 安装主题(你们可以改成自己喜欢的主题)
-## git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
+git clone -b master https://github.com/jerryc127/hexo-theme-butterfly.git themes/butterfly
 ## git clone https://github.com/next-theme/hexo-theme-next themes/next
-git clone https://github.com/79e/hexo-theme-quiet themes/quiet
+## git clone https://github.com/79e/hexo-theme-quiet themes/quiet
 # 使用新的渲染器
 
-npm i hexo-renderer-less
-npm i hexo-renderer-ejs
+## npm i hexo-renderer-less
+## npm i hexo-renderer-ejs
 # npm un hexo-renderer-marked --save
 # npm i hexo-renderer-markdown-it --save
 # npm i --save hexo-wordcount
